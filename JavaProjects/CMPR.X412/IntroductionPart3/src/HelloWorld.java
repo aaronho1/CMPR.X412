e20689f2e9bc362ad6c35e0c2df3a6d709680427
aaronho1 <38473112+aaronho1@users.noreply.github.com>
2021-04-08 15:24:25 -0700

    Add files via upload

---
+++ JavaProjects/CMPR.X412/IntroductionPart3/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	//reserve words public, static and void
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.println("$$$$$$$$$$");
+		
+		System.out.print("Hello ");
+		System.out.println("World");
+		
+		System.out.println("Hello\nWorld"); //escape character
+		
+
+	}
+}
