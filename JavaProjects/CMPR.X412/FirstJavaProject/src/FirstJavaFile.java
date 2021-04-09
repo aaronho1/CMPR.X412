5fda9d1e2695d801bc10e966c8334b59a6cd7f55
aaronho1 <38473112+aaronho1@users.noreply.github.com>
2021-04-08 21:11:48 -0700

    Add files via upload

---
+++ JavaProjects/CMPR.X412/FirstJavaProject/src/FirstJavaFile.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+
+/**
+ * @author AaronHo
+ *
+ */
+import java.util.Scanner; 
+public class FirstJavaFile {
+	// Here you write the complete code for exercise# 1
+		public static void excercise_1_0() {
+			// For homework#1, this exercise is for you to do at home, there is not submission needed 
+			// You could say like this:
+			
+			System.out.println("I completed all questions and got 90% right in first try");
+		}
+
+		// Here you write the complete code for exercise# 2
+		public static void excercise_1_1() {
+			// For homework#1, this exercise is for you to do at home, there is not submission needed 
+			// You could say like this:
+			
+			System.out.println("My Eclipse is running perfect and I am able to get started with my homework");
+		}
+
+		// Here you write the complete code for exercise# 2
+		public static void excercise_1_2() {
+			System.out.println("Hello Java World");
+			
+			//2. Now change the code so that it displays Hello in one line, and Java World in another line.
+			System.out.println("Hello");
+			System.out.println("Java World");
+			
+		}
+
+		// Here you write the complete code for exercise# 4
+		public static void excercise_1_3() {
+			// HELLO
+			
+			System.out.println("***       ***     ********    ***        ***           ***");	
+			System.out.println("***       ***     ***         ***        ***        ***   ***");		
+			System.out.println("***       ***     ***         ***        ***        ***   ***");		
+			System.out.println("*************     ********    ***        ***        ***   ***");		
+			System.out.println("***       ***     ***         ***        ***        ***   ***");
+			System.out.println("***       ***     ***         ***        ***        ***   ***");
+			System.out.println("***       ***     ********    ********   ********      ***");
+			
+		}
+
+		// Here you write the complete code for exercise# 5
+		public static void excercise_1_4() {
+			System.out.println("--------------------");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("|                  |");
+			System.out.println("--------------------");
+		}
+		
+		////more exercises more methods
+		public static void main(String[] args) {
+
+			// now call these methods
+			excercise_1_0(); 
+			excercise_1_1(); 
+			excercise_1_2(); 
+			excercise_1_3(); 
+			excercise_1_4(); 
+			////add as many as you need
+		}
+
+}
